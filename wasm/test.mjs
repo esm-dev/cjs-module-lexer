74eaa1898d34e7895467326ecb58ee7495d369e9
Je Xia <i@jex.me>
2025-12-25 11:00:25 +0800

    Bump package version to 1.0.8

--- wasm/test.mjs
+++ wasm/test.mjs
@@ -1,10 +1,18 @@
 import { parse } from "./index.mjs";
 
-console.log(parse("test.cjs", `
+const { exports, reexports } = parse("test.cjs", `
+  module.exports = require("./lib");
   exports.a = "a";
   module.exports.b = "b";
   Object.defineProperty(exports, "c", { value: 1 });
   Object.defineProperty(module.exports, "__esModule", { value: true })
   const key = "foo"
   Object.defineProperty(exports, key, { value: "e" });
-`));
+`);
+if (exports.join(",") !== "a,b,c,__esModule,foo") {
+  throw new Error("exports is expected to be a,b,c,__esModule,foo, but got " + exports.join(","));
+}
+if (reexports.join(",") !== "./lib") {
+  throw new Error("reexports is expected to be ./lib, but got " + reexports.join(","));
+}
+console.log("✅ test passed");
